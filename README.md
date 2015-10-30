5f7dbf8d13fade6d4b834532fecffdf342be0bff
Jonathan Neal <jonathantneal@hotmail.com>
2015-10-29 22:26:08 -0400

    0.4.0

--- README.md
+++ README.md
@@ -4,7 +4,40 @@
 
 [![NPM Version][npm-img]][npm]
 
-[GitHub] is a theme for [mdcss].
+[GitHub] is a Github-flavored theme for [mdcss].
+
+## Usage
+
+Add [mdcss] and [mdcss-theme-github] to your build tool:
+
+```bash
+npm install mdcss --save-dev
+npm install mdcss-theme-github --save-dev
+```
+
+#### Node
+
+```js
+require('mdcss')({
+	theme: require('mdcss-theme-github')({ /* options */ })
+}).process(YOUR_CSS);
+```
+
+## Options
+
+#### `template`
+
+Type: `String`  
+Default: `'main'`
+
+The template within the mdcss theme you would like to use.
+
+#### `index`
+
+Type: `String`  
+Default: `'index.html'`
+
+The filename of the main file you would like generated inside the style guide directory.
 
 [npm]:     https://www.npmjs.com/package/mdcss-theme-github
 [npm-img]: https://img.shields.io/npm/v/mdcss-theme-github.svg
--- index.js
+++ index.js
@@ -2,19 +2,26 @@ var ejs  = require('ejs');
 var fs   = require('fs-wishlist').mixin(require('fs-promise'));
 var path = require('path');
 
-module.exports = function (section, opts) {
-	var directory = path.join(process.cwd(), opts.directory);
-	var index     = path.join(directory, opts.index);
+module.exports = function (opts) {
+	opts = opts || {};
 
-	var assets   = path.join(__dirname, 'assets');
-	var template = path.join(__dirname, opts.template);
+	opts.template  = opts.template || 'main';
+	opts.index     = opts.index    || 'index.html';
 
-	return Promise.all([
-		fs.copy(assets, directory),
-		fs.readFile(template, 'utf8').then(function (contents) {
-			var compiled = ejs.compile(contents)(section);
+	return {
+		process: function (documentation, destination) {
+			var index    = path.join(destination, opts.index);
+			var assets   = path.join(__dirname, 'assets');
+			var template = path.join(__dirname, opts.template + '.ejs');
 
-			return fs.writeFile(index, compiled);
-		})
-	]);
+			return Promise.all([
+				fs.copy(assets, destination),
+				fs.readFile(template, 'utf8').then(function (contents) {
+					var compiled = ejs.compile(contents)(documentation);
+
+					return fs.writeFile(index, compiled);
+				})
+			]);
+		}
+	};
 };
