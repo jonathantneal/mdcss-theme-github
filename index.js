655d244e913aa6ea3d53fa3161ebbc2481a9fda6
Chris <orthagonal1@gmail.com>
2016-09-01 17:53:03 -0500

    groups colors by value

--- index.js
+++ index.js
@@ -54,7 +54,19 @@ module.exports = function (themeopts) {
 
     // set theme options
     docs.themeopts = themeopts;
-
+    const sortColors = (unsortedColors) => {
+      return Object.keys(unsortedColors).sort((colorA, colorB) => {
+        const aV = unsortedColors[colorA];
+        const bV = unsortedColors[colorB];
+        if (aV > bV) {
+          return 1;
+        }
+        if (bV > aV) {
+          return -1;
+        }
+        return 0;
+      });
+    }
     if (themeopts.colors || themeopts.variables) {
       const styleguide = {
         title: 'Styleguide',
@@ -66,7 +78,7 @@ module.exports = function (themeopts) {
           section: 'Styleguide',
           title: 'Colors',
           name: 'colors',
-          content: Object.keys(themeopts.colors).map((color) => (
+          content: sortColors(themeopts.colors).map((color) => (
             `<div class="color-swatch" style="background-color: ${themeopts.colors[color]}; color: ${fontContrast(themeopts.colors[color])}; display: inline-block; height: 75px; width: 150px;"><div class="color-value">${themeopts.colors[color]}</div><div class="color-name">${color}</div></div>`
           )).join('')
         });
